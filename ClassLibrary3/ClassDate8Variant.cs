332e7bfa52b8566d7f230fc14cf8bcece501ccb4
Egor Lavrov <148620130+Lavrikk@users.noreply.github.com>
2024-05-02 12:49:17 +0300

    Update ClassDate8Variant.cs

--- ClassLibrary3/ClassDate8Variant.cs
+++ ClassLibrary3/ClassDate8Variant.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-
-
-
 namespace ClassLibaryTPO8VAR
 {
     public class Smena
     {
         private string _nazvanie;
-
         public Smena(string nazvanie, DateTime? date = null)
         {
             _nazvanie = nazvanie;
             if (date == null)
             {
                 date = DateTime.Now;
-            }
-            
+            }   
         }
     }
 }
-
